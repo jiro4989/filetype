6a388f74258b547500dee3097aa527bef193bff1
jiro4989 <jiroron666@gmail.com>
2020-12-25 09:47:25 +0900

    chore: add documentation comment

--- src/filetype.nim
+++ src/filetype.nim
@@ -2,6 +2,7 @@ import filetype/[types, image]
 export types
 
 proc match*(buf: openArray[byte]): FileType =
+  ## Returns a file type from matched magic number.
   for matcher in imageMatcher:
     let matchFunc = matcher[1]
     if buf.matchFunc:
@@ -11,6 +12,10 @@ when not defined js:
   import streams
 
   proc matchFile*(file: string): FileType =
+    ## Returns a file type from matched magic number.
+    ## `file` is a file path.
+    ##
+    ## **Note:** Not available for JS backend.
     var strm = openFileStream(file)
     defer: strm.close()
     var buf: array[20, byte]
