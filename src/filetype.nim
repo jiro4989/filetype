d6e40d855e7c7ef3d9453b39b4d2c3d186beb188
jiro4989 <jiroron666@gmail.com>
2020-12-25 09:17:35 +0900

    add matchfile

--- src/filetype.nim
+++ src/filetype.nim
@@ -1,8 +1,18 @@
 import filetype/[types, image]
 export types
 
-proc match*(buf: seq[byte]): FileType =
+proc match*(buf: openArray[byte]): FileType =
   for matcher in imageMatcher:
     let matchFunc = matcher[1]
     if buf.matchFunc:
       return matcher[0]
+
+when not defined js:
+  import streams
+
+  proc matchFile*(file: string): FileType =
+    var strm = openFileStream(file)
+    defer: strm.close()
+    var buf: array[20, byte]
+    strm.read(buf)
+    return buf.match()
