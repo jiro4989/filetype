cc7ff29fe728f34bf732c0f8d10ffc2bfec3963d
jiro4989 <jiroron666@gmail.com>
2020-12-25 09:36:42 +0900

    chore: add testcode

--- tests/test1.nim
+++ tests/test1.nim
@@ -1,7 +1,12 @@
-import unittest
+import unittest, os
 
 include filetype
 
-suite "suite":
-  test "test":
-    check true
+const
+  testDataDir = "tests"/"testdata"
+
+suite "proc matchFile":
+  test "png": check matchFile(testDataDir/"sample.png").mime.value == "image/png"
+  test "jpeg": check matchFile(testDataDir/"sample.jpeg").mime.value == "image/jpeg"
+  test "jpg": check matchFile(testDataDir/"sample.jpg").mime.value == "image/jpeg"
+  test "gif": check matchFile(testDataDir/"sample.gif").mime.value == "image/gif"
