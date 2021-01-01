c617607bb09ca66ce11747354c4632d3282bbdd9
jiro4989 <jiroron666@gmail.com>
2021-01-01 22:21:37 +0900

    chore: update testcode

--- tests/test1.nim
+++ tests/test1.nim
@@ -5,12 +5,15 @@ include filetype
 const
   testDataDir = "tests"/"testdata"
 
+func sampleFile(ext: string): string =
+  return testDataDir/"sample." & ext
+
 suite "proc matchFile":
-  test "png": check matchFile(testDataDir/"sample.png").mime.value == "image/png"
-  test "jpeg": check matchFile(testDataDir/"sample.jpeg").mime.value == "image/jpeg"
-  test "jpg": check matchFile(testDataDir/"sample.jpg").mime.value == "image/jpeg"
-  test "gif": check matchFile(testDataDir/"sample.gif").mime.value == "image/gif"
-  test "gzip": check matchFile(testDataDir/"sample.gz").mime.value == "application/gzip"
-  test "zip": check matchFile(testDataDir/"sample.zip").mime.value == "application/zip"
-  test "midi": check matchFile(testDataDir/"sample.mid").mime.value == "audio/midi"
-  test "wav": check matchFile(testDataDir/"sample.wav").mime.value == "audio/x-wav"
+  test "png": check matchFile(sampleFile("png")).mime.value == "image/png"
+  test "jpeg": check matchFile(sampleFile("jpeg")).mime.value == "image/jpeg"
+  test "jpg": check matchFile(sampleFile("jpg")).mime.value == "image/jpeg"
+  test "gif": check matchFile(sampleFile("gif")).mime.value == "image/gif"
+  test "gzip": check matchFile(sampleFile("gz")).mime.value == "application/gzip"
+  test "zip": check matchFile(sampleFile("zip")).mime.value == "application/zip"
+  test "midi": check matchFile(sampleFile("mid")).mime.value == "audio/midi"
+  test "wav": check matchFile(sampleFile("wav")).mime.value == "audio/x-wav"
