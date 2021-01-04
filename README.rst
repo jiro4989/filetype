7572288fecc90dbf549f90f48a98767caf5d34f0
jiro4989 <jiroron666@gmail.com>
2021-01-04 21:57:38 +0900

    add cr2

--- README.rst
+++ README.rst
@@ -32,7 +32,7 @@ Supported format
 =======   ======
 Type      Format
 =======   ======
-Image     jpeg, jp2, png, gif, webp, bmp, jxr, psd, ico, dwg
+Image     jpeg, jp2, png, gif, webp, cr2, bmp, jxr, psd, ico, dwg
 Archive   gz, zip, bz2, 7z, pdf, exe, rtf, nes, crx, ps, xz, sqlite, deb, ar, lz, rpm, elf
 Audio     midi, ogg, flac, wav, amr, mp3, aac
 Font      woff, woff2, ttf, otf
--- src/filetype/image.nim
+++ src/filetype/image.nim
@@ -51,6 +51,14 @@ func isWebp*(buf: openArray[byte]): bool =
     buf[10] == 0x42'u8 and
     buf[11] == 0x50'u8
 
+func isCr2*(buf: openArray[byte]): bool =
+  return 10 < buf.len and
+    ((buf[0] == 0x49'u8 and buf[1] == 0x49'u8 and buf[2] == 0x2a'u8 and buf[3] == 0x0'u8) or
+    (buf[0] == 0x4d'u8 and buf[1] == 0x4d'u8 and buf[2] == 0x0'u8 and buf[3] == 0x2a'u8) ) and
+    buf[8] == 0x43'u8 and
+    buf[9] == 0x52'u8 and
+    buf[10] == 0x02'u8
+
 const
   imageMatcher* = @[
     (typeJpeg, isJpeg),
@@ -58,6 +66,7 @@ const
     (typePng, isPng),
     (typeGif, isGif),
     (typeWebp, isWebp),
+    (typeCr2, isCr2),
     (typeBmp, isBmp),
     (typeJxr, isJxr),
     (typePsd, isPsd),
