4a1b51e1d5662fc52c890ad18de995bc1b4df8de
jiro4989 <jiroron666@gmail.com>
2021-01-21 00:37:09 +0900

    add webp test

--- tests/test1.nim
+++ tests/test1.nim
@@ -9,11 +9,15 @@ func sampleFile(ext: string): string =
   return testDataDir/"sample." & ext
 
 suite "proc matchFile":
-  test "png": check matchFile(sampleFile("png")).mime.value == "image/png"
-  test "jpeg": check matchFile(sampleFile("jpeg")).mime.value == "image/jpeg"
+  # image
   test "jpg": check matchFile(sampleFile("jpg")).mime.value == "image/jpeg"
+  test "jpeg": check matchFile(sampleFile("jpeg")).mime.value == "image/jpeg"
+  test "png": check matchFile(sampleFile("png")).mime.value == "image/png"
   test "gif": check matchFile(sampleFile("gif")).mime.value == "image/gif"
+  test "webp": check matchFile(sampleFile("webp")).mime.value == "image/webp"
+  # archive
   test "gzip": check matchFile(sampleFile("gz")).mime.value == "application/gzip"
   test "zip": check matchFile(sampleFile("zip")).mime.value == "application/zip"
+  # audio
   test "midi": check matchFile(sampleFile("mid")).mime.value == "audio/midi"
   test "wav": check matchFile(sampleFile("wav")).mime.value == "audio/x-wav"
