dbb5b3c2fc75b18c96a77d1001ecd1bc9dd0f3f4
jiro4989 <jiroron666@gmail.com>
2021-01-21 00:39:11 +0900

    add tiff test

--- tests/test1.nim
+++ tests/test1.nim
@@ -15,6 +15,7 @@ suite "proc matchFile":
   test "png": check matchFile(sampleFile("png")).mime.value == "image/png"
   test "gif": check matchFile(sampleFile("gif")).mime.value == "image/gif"
   test "webp": check matchFile(sampleFile("webp")).mime.value == "image/webp"
+  test "tiff": check matchFile(sampleFile("tiff")).mime.value == "image/tiff"
   # archive
   test "gzip": check matchFile(sampleFile("gz")).mime.value == "application/gzip"
   test "zip": check matchFile(sampleFile("zip")).mime.value == "application/zip"
