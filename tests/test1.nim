2956291a3fae0167d17cfa03f15798d6e988152a
jiro4989 <jiroron666@gmail.com>
2021-01-22 00:01:55 +0900

    add avi test

--- tests/test1.nim
+++ tests/test1.nim
@@ -49,6 +49,7 @@ suite "proc matchFile":
   # video
   test "video: wmv": check matchFile(sampleFile("wmv")).mime.value == "video/x-ms-wmv"
   test "video: mp4": check matchFile(sampleFile("mp4")).mime.value == "video/mp4"
+  test "video: avi": check matchFile(sampleFile("avi")).mime.value == "video/x-msvideo"
 
   # others
   test "returns empty when a file doesn't exist": check matchFile("hello.world").mime.value == ""
