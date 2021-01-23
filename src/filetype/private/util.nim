3990eb3a72b43b5828db1164f399f5f1caa54e19
jiro4989 <jiroron666@gmail.com>
2021-01-23 13:33:30 +0900

    fix

--- src/filetype/private/util.nim
+++ src/filetype/private/util.nim
@@ -9,13 +9,42 @@ func checkMagicNumber*(buf, magicNumber: openArray[byte]): bool =
 func str2Bytes*(s: string): seq[byte] =
   toSeq(s.items).mapIt(it.byte)
 
+when not defined js:
+  import streams
+  from os import getFileSize, fileExists
+  from sequtils import newSeqWith
+
+  proc readMagicNubmer*(file: string): seq[byte] =
+    if not fileExists(file): return
+
+    const maxSize = 1024
+    let fileSize = file.getFileSize()
+    let size =
+      if fileSize < maxSize: fileSize.int
+      else: maxSize
+
+    var strm = openFileStream(file)
+    defer: strm.close()
+    var buf = newSeqWith(size, 0'u8)
+    for i in 0..<size:
+      buf[i] = strm.readUint8()
+    return buf
+
 macro generateFunc*(format: untyped): untyped =
-  let funcName = newIdentNode("is" & $format)
-  let constName = newIdentNode("magicNumber" & $format)
-  let arg = newIdentNode("buf")
-  let comment = newCommentStmtNode(&"Returns `buf` matches {format} format magic number.")
+  let
+    funcName = newIdentNode("is" & $format)
+    constName = newIdentNode("magicNumber" & $format)
+    arg = newIdentNode("buf")
+    comment = newCommentStmtNode(&"Returns `buf` matches {format} format magic number.")
+
+    readFileFuncName = newIdentNode("is" & $format & "File")
+    fileArg = newIdentNode("file")
+
   quote do:
     func `funcName`*(`arg`: openArray[byte]): bool =
       `comment`
       checkMagicNumber(`arg`, `constName`)
 
+    when not defined js:
+      proc `readFileFuncName`*(`fileArg`: string): bool =
+        readMagicNubmer(`fileArg`).`funcName`()
